2167c3156b0b2b135dc5113b4b31f3e14b8205ee
Rizwan Ali Sabir <rizwanA@expertdynamicsgroup.com>
2023-02-09 15:57:19 +0500

    dummy

---
+++ WebApplication1/TestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Controllers;
+
+namespace TestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var controller = new HomeController(null);
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
